0e41c7f442a40dc29c1b859a9d1de65d351c5f3a
Jordan Zehr <jordan.zehr@temple.edu>
2018-05-11 11:04:37 -0400

    time points 1,9,18,27 (file writer for fasta file)

--- pipeline.sh
+++ pipeline.sh
@@ -1,8 +1,8 @@
 # Extract fasta from JSON
-python extract_fasta_from_json.py --file data/6_clone.json --size 30
-python extract_fasta_from_json.py --file data/7_clone.json --size 30
+python extract_fasta_from_json.py --file data/1_clone.json --size 30
 python extract_fasta_from_json.py --file data/9_clone.json --size 30
-python extract_fasta_from_json.py --file data/11_clone.json --size 30
+python extract_fasta_from_json.py --file data/18_clone.json --size 30
+python extract_fasta_from_json.py --file data/27_clone.json --size 30
 
 cat data/*clone*_unaligned.fasta > data/full_size-30_unaligned.fasta
 mafft data/full_size-30_unaligned.fasta > data/full_size-30.fasta
