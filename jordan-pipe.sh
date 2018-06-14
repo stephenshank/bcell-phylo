8bfbffd2e6f4914cde894bd35ad912c37e550023
Jordan Zehr <jordan.zehr@temple.edu>
2018-06-14 14:03:41 -0400

    added bcell-pipe-structure.sh which will become the pipeline script

--- jordan-pipe.sh
+++ jordan-pipe.sh
@@ -1,7 +1,7 @@
 ## this pipeline will run the modules one by one if need be. this was just cleaner than having one large bash file.
 ## eventually this will be a snakemake file or something similar
 
-bash modules/remover.sh
+#bash modules/remover.sh
 
 bash modules/json_to_fasta.sh
 ##################################
@@ -9,37 +9,37 @@ bash modules/json_to_fasta.sh
 ## output = unaligned fasta files 
 #################################
 
-bash modules/cat_unaligned.sh
+#bash modules/cat_unaligned.sh
 #################################################################
 ## input = all unaligned json timepoint fasta files
 ## output = unaligned fasta files with all time points together 
 ################################################################
 
-bash modules/get_v-genes.sh
+#bash modules/get_v-genes.sh
 ######################################################
 ## input = full unaligned time-point json fasta file 
 ## output = each unique v gene unaligned fasta files
 #####################################################
 
-bash modules/aligner.sh
+#bash modules/aligner.sh
 ##############################################
 ## input = unaligned_jsons, unaligned_v-genes
 ## output = aligned_jsons time points, aligned_v-genes across all time points**
 ############################################
 
-bash modules/tsv_maker.sh
+#bash modules/tsv_maker.sh
 #############################
 ## input = full aligned json 
 ## output = tsv file 
 #############################
 
-bash modules/tree-builder.sh
+#bash modules/tree-builder.sh
 #############################################
 ## input = all aligned all jsons and v-genes
 ## output = trees in newish format 
 ############################################
 
-bash modules/ggplotter.sh
+#bash modules/ggplotter.sh
 ##################################
 ## input = tsv file made earlier
 ## output = pretty png files
--- modules/aligner.sh
+++ modules/aligner.sh
@@ -4,18 +4,18 @@
 mafft data/sandbox/output/unaligned_jsons/full_clone_size-30_unaligned.fasta > data/sandbox/output/aligned_jsons/full_aligned.fasta
 
 	## this loop is going to align all the time-point fasta files ##
-for i in {7..12}
-do
-  mafft data/sandbox/output/unaligned_jsons/$i\_clone_size-30_unaligned.fasta > data/sandbox/output/aligned_jsons/$i\_aligned.fasta
+#for i in {7..12}
+#do
+#  mafft data/sandbox/output/unaligned_jsons/$i\_clone_size-30_unaligned.fasta > data/sandbox/output/aligned_jsons/$i\_aligned.fasta
 
-done;
+#done;
 
 	## this is aligning the first V gene that (does not get assigned?)## 
 mafft data/sandbox/output/unaligned_v-genes/V_un.fasta > data/sandbox/output/aligned_v-genes/V_aligned.fasta
 
 	## this loop grabs all the unaligned gene files and then aligns them ##
-for i in {1..7}
-do
-  mafft data/sandbox/output/unaligned_v-genes/V$i\_un.fasta > data/sandbox/output/aligned_v-genes/V$i\_aligned.fasta
+#for i in {1..7}
+#do
+mafft data/sandbox/output/unaligned_v-genes/V$i\_un.fasta > data/sandbox/output/aligned_v-genes/V$i\_aligned.fasta
 
-done;
+#done;
