39422ab57386a6999cd59b08c6afed18f53a1caa
Stephen Shank <sshank314@gmail.com>
2018-08-01 09:13:08 -0400

    Highlighting regions, shuffling through all patients/genes.

--- js/index.js
+++ js/index.js
@@ -13,20 +13,24 @@ require('./main.css');
 class App extends Component {
   constructor(props){
     super(props);
-    this.patients = [ '77612', '67029' ];
-    this.genes = [1,2,3,4,5,6,7].map(i=>'V'+i);
+    this.patients = [ "28729", "48689", "67029", "77612", "78202", "93954", "99361", "99682", "GJS" ];
+    this.genes = [1,2,3,4,5,6].map(i=>'V'+i);
     this.state = { 
       patient: null,
       gene: null
     };
   }
   loadData(patient, gene) {
-    const json_path = `/data/out/${patient}/${gene}.json`;
-    d3.json(json_path, (err, json_data) => {
-      this.setState({
-        patient: patient,
-        gene: gene,
-        json: json_data
+    this.setState({json: null}, function() {
+      const json_path = `/data/out/${patient}/${gene}.json`;
+      d3.json(json_path, (err, json_data) => {
+        json_data.patient = patient;
+        json_data.gene = gene;
+        this.setState({
+          patient: patient,
+          gene: gene,
+          json: json_data
+        });
       });
     });
   }
