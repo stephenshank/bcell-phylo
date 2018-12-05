584e510b0a45d7fd10090b40b92524f62935c230
stephenshank <sshank314@gmail.com>
2018-12-04 20:06:01 -0500

    Using React Table for overview.

--- js/overview.js
+++ js/overview.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react';
+import ReactTable from "react-table";
+const d3 = require("d3");
+import 'react-table/react-table.css'
 
-function Overview(props) {
-  return <h1>This will be an overview.</h1>;
+class Overview extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: null };
+  }
+  componentDidMount() {
+    d3.csv('data/cluster_information.csv', (error, data) => {
+      data.forEach(row=>{
+        row.visit1 = +row.visit1;
+        row.visit2 = +row.visit2;
+        row.visit3 = +row.visit3;
+      });
+      this.setState({data: data});
+    });
+  }
+  render() {
+    return (<div>
+      {this.state.data ? <ReactTable
+        data={this.state.data}
+        columns={[
+          { Header: "Patient ID", accessor: 'patient_id'},
+          { Header: "V-gene allele", accessor: 'vgene_allele'},
+          { Header: "Visit 1 size", accessor: 'visit1'},
+          { Header: "Visit 2 size", accessor: 'visit2'},
+          { Header: "Visit 3 size", accessor: 'visit3'}
+        ]}
+      /> : null }
+    </div>);
+  }
 }
 
 module.exports = Overview;
